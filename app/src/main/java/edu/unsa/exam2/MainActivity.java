23be58288c45d16a287ec630c3b8bb175e0c01bf
KuroNeko <nekitoedmh@gmail.com>
2021-07-03 22:33:49 -0500

    ADD Android Activity Recognition Feature

--- app/src/main/java/edu/unsa/exam2/MainActivity.java
+++ app/src/main/java/edu/unsa/exam2/MainActivity.java
@@ -1,26 +1,33 @@
 package edu.unsa.exam2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import edu.unsa.exam2.model.Filter;
+import android.widget.Toast;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import edu.unsa.exam2.model.FilterHighPass;
 import edu.unsa.exam2.model.StepsStorage;
 import edu.unsa.exam2.service.MqttConfig;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final int MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 20;
 
     SensorManager sensor_manager;
     Sensor acelerometer;
@@ -38,13 +45,29 @@ public class MainActivity extends AppCompatActivity {
     Button exit;
     Button restart;
     boolean on;
+    boolean on_2;
 
     MqttConfig mqtt_config;
+    private Sensor stepCounterSensor;
+    // Resetear a cero el boton con el boton
+    private int step = 0;
 
+    @SuppressLint("ShowToast")
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Google Activity Recognition
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACTIVITY_RECOGNITION},
+                    MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION);
+        }
+
 
         tv_test_a = (TextView) findViewById(R.id.label_test_a);
         tv_test_s = (TextView) findViewById(R.id.label_test_s);
@@ -55,8 +78,18 @@ public class MainActivity extends AppCompatActivity {
         progressBarA = (ProgressBar) findViewById(R.id.progressBarA);
         progressBarS = (ProgressBar) findViewById(R.id.progressBarS);
 
-
         sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // Incicialización de contador de pasos proporcionado por la api de android.
+        stepCounterSensor = sensor_manager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        if (stepCounterSensor == null) {
+            Toast.makeText(this, "No tiene el sensor", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Tiene el sensor", Toast.LENGTH_LONG).show();
+        }
+
+        // =================================================================
+
         acelerometer = sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         mqtt_config = new MqttConfig(this.getApplicationContext());
@@ -66,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSensorChanged(SensorEvent event) {
                 if (event != null) {
                     //actualGeneralAceleration = Filter.getGeneralAceleration(event);
-                    actualGeneralAceleration= FilterHighPass.getGeneralAceleration(event);
+                    actualGeneralAceleration = FilterHighPass.getGeneralAceleration(event);
                 }
             }
 
@@ -78,9 +111,10 @@ public class MainActivity extends AppCompatActivity {
         sensor_manager.registerListener(stepDetector, acelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         on = false;
     }
-    public void restartValues(View v){
-        tv_test_a.setText(0+"");
-        tv_test_s.setText(0+"");
+
+    public void restartValues(View v) {
+        tv_test_a.setText(0 + "");
+        tv_test_s.setText(0 + "");
         progressBarA.setProgress(0);
         progressBarS.setProgress(0);
     }
@@ -94,12 +128,20 @@ public class MainActivity extends AppCompatActivity {
             start_stop_a.setText("Detener\nAcelerometro");
         }
     }
+
     public void startStopFunctionSen(View v) {
-        if (on) {
-            pauseFunction();
+        if (on_2) {
+            on_2 = false;
+            step = 0;
+            if (stepCounterSensor != null) {
+                sensor_manager.unregisterListener(this, stepCounterSensor);
+            }
             start_stop_s.setText("Iniciar\nSensor");
         } else {
-            resumeFunction();
+            if (stepCounterSensor != null) {
+                sensor_manager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+            on_2 = true;
             start_stop_s.setText("Detener\nSensor");
         }
     }
@@ -145,12 +187,27 @@ public class MainActivity extends AppCompatActivity {
             protected void onProgressUpdate(Object[] values) {
                 super.onProgressUpdate(values);
                 TextView acimut = findViewById(R.id.label_test_a);
-                acimut.setText(StepsStorage.getStepsCount()+"");
+                acimut.setText(StepsStorage.getStepsCount() + "");
                 progressBarA.setProgress(StepsStorage.getStepsCount());
 
             }
         };
         on = true;
         tarea.execute();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == stepCounterSensor) {
+            step += (int) sensorEvent.values[0];
+            tv_test_s.setText("" + step);
+            mqtt_config.publishMessage(String.valueOf(step));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing method
     }
 }
